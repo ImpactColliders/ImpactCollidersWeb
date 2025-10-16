edfdc4126126a83b04a04ae8e23f08418c9881a0
WeirdOne999 <106362960+WeirdOne999@users.noreply.github.com>
2025-10-16 13:27:27 +0800

    changes

--- contactus.js
+++ contactus.js
@@ -32,9 +32,9 @@ function createContactForm(containerId) {
             <h2>Contact Us</h2>
             <ul class="social">
               <li><a href="https://www.instagram.com/impactcolliders/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+              <li><a href="https://www.tiktok.com/@impactcolliders?is_from_webapp=1&sender_device=pc" target="_blank"><i class="fa fa-tiktok"><img src="assets/images/tiktok.png" style="height:2dvh"></i></a></li>
               <li><a href="https://www.facebook.com/p/Impact-Colliders-61571335654695/" target="_blank"><i class="fa fa-facebook"></i></a></li>
               <li><a href="https://www.linkedin.com/company/impact-colliders/posts/?feedView=all" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-              <li><a href="https://www.tiktok.com/@impactcolliders?is_from_webapp=1&sender_device=pc" target="_blank"><i class="fa fa-tiktok"><img src="assets/images/tiktok.png" style="height:2dvh"></i></a></li>
             </ul>
           </div>
         </div>
@@ -43,29 +43,55 @@ function createContactForm(containerId) {
 
         <br>
   
-<div id="mc_embed_signup">
-    <form action="https://impactcolliders.us10.list-manage.com/subscribe/post?u=1b49b4b873f4789130f22dfd4&amp;id=c856daa883&amp;f_id=008cc2e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
-        <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
-            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div><div class="mc-field-group"><label for="mce-FNAME">First Name <span class="asterisk">*</span></label><input type="text" name="FNAME" class="required text" id="mce-FNAME" required="" value=""></div><div class="mc-field-group"><label for="mce-LNAME">Last Name <span class="asterisk">*</span></label><input type="text" name="LNAME" class="required text" id="mce-LNAME" required="" value=""></div>
-<div hidden=""><input type="hidden" name="tags" value="357"></div>
-        <div id="mce-responses" class="clear foot">
-            <div class="response" id="mce-error-response" style="display: none;"></div>
-            <div class="response" id="mce-success-response" style="display: none;"></div>
-        </div>
-    <div aria-hidden="true" style="position: absolute; left: -5000px;">
-        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
-        <input type="text" name="b_1b49b4b873f4789130f22dfd4_c856daa883" tabindex="-1" value="">
-    </div>
-        <div class="optionalParent">
-            <div class="clear foot">
-                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
-            </div>
-        </div>
-    </div>
+<!-- Impact Colliders Mailing List -->
+<form
+  action="https://impactcolliders.us10.list-manage.com/subscribe/post?u=1b49b4b873f4789130f22dfd4&amp;id=c856daa883&amp;f_id=008cc2e1f0"
+  method="post"
+  target="_self"
+  style="max-width:420px;margin:20px 0;padding:20px;border:1px solid #ccc;border-radius:8px;font-family:sans-serif;text-align:left;"
+>
+  <h2 style="text-align:left;margin-bottom:16px;">Join our Mailing List</h2>
+
+  <label>Email Address</label>
+  <input
+    type="email"
+    name="EMAIL"
+    required
+    style="width:100%;padding:10px;margin:6px 0 12px;border:1px solid #ccc;border-radius:4px;"
+  >
+
+  <label>First Name</label>
+  <input
+    type="text"
+    name="FNAME"
+    required
+    style="width:100%;padding:10px;margin:6px 0 12px;border:1px solid #ccc;border-radius:4px;"
+  >
+
+  <label>Last Name</label>
+  <input
+    type="text"
+    name="LNAME"
+    required
+    style="width:100%;padding:10px;margin:6px 0 16px;border:1px solid #ccc;border-radius:4px;"
+  >
+
+  <input type="hidden" name="tags" value="357">
+
+  <!-- honeypot (anti-bot) -->
+  <div style="position:absolute;left:-5000px;" aria-hidden="true">
+    <input type="text" name="b_1b49b4b873f4789130f22dfd4_c856daa883" tabindex="-1" value="">
+  </div>
+
+  <button
+    type="submit"
+    style="width:100%;padding:10px;background:#0073e6;color:#fff;border:none;border-radius:4px;cursor:pointer;"
+  >
+    Subscribe
+  </button>
 </form>
-</div>
-</div>
+
+
 
 
         <div class="sub-footer">
