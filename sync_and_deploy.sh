d1b2c240987ee4fe1492bd070a8e765a22c77464
Ong Yong Xin <ongyongxin2020+github@gmail.com>
2025-11-21 18:35:11 +0800

    misc: Add shell script for deploying website

---
+++ sync_and_deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+export AWS_PAGER = ""
+export AWS_DEFAULT_OUTPUT = "json"
+
+echo "[*] Syncing content/"
+aws s3 sync \
+    content s3://impact-colliders/content
+
+echo "[*] Syncing website/"
+aws s3 sync \
+    --exclude ".git/*" \
+    --exclude "content/*" \
+    --exclude "*.pem" \
+    --exclude "*.ps1" \
+    --delete  \
+    . s3://impact-colliders/website
+
+echo "[*] Invalidating cloudfront cache"
+aws cloudfront create-invalidation \
+    --distribution-id E96C4MHLZ4061 \
+    --paths "/*"
+
+echo "[*] Deploying website"
+aws amplify start-deployment \
+    --app-id d3dt3h2w2phi5b \
+    --branch production \
+    --source-url s3://impact-colliders/website/ \
+    --source-url-type BUCKET_PREFIX
